5d60d9154844dec5fbef6b6ddcaa2e85c35589b2
Michael Okoro <michaelokoro1@gmail.com>
2024-02-04 17:59:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Load the saved model
 try:
@@ -23,32 +21,36 @@ page = st.sidebar.selectbox("Navbar", ["Prediction", "Performance Analysis"])
 if page == "Prediction":
     # Upload image through Streamlit
     uploaded_file = st.file_uploader("Choose a test image...", type=["jpg", "jpeg", "png"])
-    
+
     # Model performance analysis
     if uploaded_file is not None:
-        # Load and preprocess the test image
-        test_image = image.load_img(uploaded_file, target_size=(150, 150))
-        test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis=0)
-        test_image = test_image / 255.0  # Normalize
-    
-        # Perform inference for prediction
-        predictions = model.predict(test_image)
-        predicted_class_index = np.argmax(predictions)
-        predicted_class_label = class_labels[predicted_class_index]
-        predicted_class_probability = predictions[0][predicted_class_index] * 100
-
         # Display the uploaded image
         st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
 
+        # Load and preprocess the test image
+        st.write("Processing the image...")
+        test_image = image.load_img(uploaded_file, target_size=(150, 150))
+        st.image(test_image, caption="Processed Image", use_column_width=True)
+        
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        test_image = test_image / 255.0  # Normalize
+        
+        # Perform inference for prediction
+        st.write("Performing inference...")
+        predictions = model.predict(test_image)
+        
         # Display probability scores for each class
         st.write("Class Probabilities:")
         for label, probability in zip(class_labels, predictions[0] * 100):
             st.write(f"{label}: {probability:.2f}%")
-
+        
         # Print the classification label with probability
+        predicted_class_index = np.argmax(predictions)
+        predicted_class_label = class_labels[predicted_class_index]
+        predicted_class_probability = predictions[0][predicted_class_index] * 100
         st.success(f'Predicted Class: {predicted_class_label} with {predicted_class_probability:.2f}% probability')
-
+        
 elif page == "Performance Analysis":
     # Perform inference for performance analysis
     st.subheader("Model Performance Analysis")
